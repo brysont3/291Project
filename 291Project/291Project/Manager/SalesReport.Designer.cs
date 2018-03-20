ebaa45baebc7e97e615a7aaa5a1061588ead7fd3
Robert Sullivan <sullivanr5@mymacewan.ca>
2018-03-19 18:48:47 -0600

    Moved AddCustomer to the Employee file and added SalesReport that gets the income of payments of a user inputted month and year

---
+++ 291Project/291Project/Manager/SalesReport.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace _291Project.Manager
+{
+    partial class SalesReport
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.MonthComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.YearComboBox = new System.Windows.Forms.ComboBox();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.dataSet1 = new _291Project.dataSet1();
+            this.paymentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.paymentTableAdapter = new _291Project.dataSet1TableAdapters.PaymentTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paymentBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MonthComboBox
+            // 
+            this.MonthComboBox.FormattingEnabled = true;
+            this.MonthComboBox.Items.AddRange(new object[] {
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December"});
+            this.MonthComboBox.Location = new System.Drawing.Point(150, 35);
+            this.MonthComboBox.Name = "MonthComboBox";
+            this.MonthComboBox.Size = new System.Drawing.Size(121, 21);
+            this.MonthComboBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select A Month";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(114, 204);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(250, 132);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Select A Year";
+            // 
+            // YearComboBox
+            // 
+            this.YearComboBox.FormattingEnabled = true;
+            this.YearComboBox.Location = new System.Drawing.Point(150, 74);
+            this.YearComboBox.Name = "YearComboBox";
+            this.YearComboBox.Size = new System.Drawing.Size(121, 21);
+            this.YearComboBox.TabIndex = 3;
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(195, 141);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(75, 23);
+            this.SubmitButton.TabIndex = 5;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.SubmitButton_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "dataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // paymentBindingSource
+            // 
+            this.paymentBindingSource.DataMember = "Payment";
+            this.paymentBindingSource.DataSource = this.dataSet1;
+            // 
+            // paymentTableAdapter
+            // 
+            this.paymentTableAdapter.ClearBeforeFill = true;
+            // 
+            // SalesReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 408);
+            this.Controls.Add(this.SubmitButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.YearComboBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.MonthComboBox);
+            this.Name = "SalesReport";
+            this.Text = "SalesReport";
+            this.Load += new System.EventHandler(this.SalesReport_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paymentBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox MonthComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox YearComboBox;
+        private System.Windows.Forms.Button SubmitButton;
+        private dataSet1 dataSet1;
+        private System.Windows.Forms.BindingSource paymentBindingSource;
+        private dataSet1TableAdapters.PaymentTableAdapter paymentTableAdapter;
+    }
+}
---
+++ 291Project/291Project/Manager/SalesReport.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _291Project.Manager
+{
+    public partial class SalesReport : Form
+    {
+        public SalesReport()
+        {
+            InitializeComponent();
+        }
+
+        private void SalesReport_Load(object sender, EventArgs e)
+        {
+            MonthComboBox.SelectedIndex = 0;
+            // TODO: This line of code loads data into the 'dataSet1.Payment' table. You can move, or remove it, as needed.
+            this.paymentTableAdapter.Fill(this.dataSet1.Payment);
+
+            //Load all the years that are present in Payments into the YearComboBox
+            string str = Properties.Settings.Default.DefaultConnection;
+
+            SqlConnection con = new SqlConnection
+            {
+                ConnectionString = str
+            };
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT DISTINCT DATEPART(Year, Date) as Years FROM Payment WHERE DATEPART(Year, Date) IS NOT NULL", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "Year");
+            YearComboBox.DisplayMember = "Years";
+            YearComboBox.ValueMember = "Years";
+            YearComboBox.DataSource = ds.Tables["Year"];
+            
+
+        }
+
+        //Updates gridView1 when the submit button is clicked
+        private void SubmitButton_Click(object sender, EventArgs e)
+        {
+            string str = Properties.Settings.Default.DefaultConnection;
+
+            SqlConnection con = new SqlConnection
+            {
+                ConnectionString = str
+            };
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT SUM(NumAccounts.AccountCounts*A.Cost) as Income FROM dbo.AccountInfo AS A, " +
+                "(SELECT P.AccountType, COUNT(P.AccountType) AS AccountCounts FROM dbo.Payment AS P WHERE DATEPART(MONTH, P.Date) = @Month " +
+                " AND DATEPART(YEAR, P.Date) = @Year " +
+                "GROUP BY P.AccountType) AS NumAccounts WHERE A.AccountType = NumAccounts.AccountType", con);
+            cmd.Parameters.AddWithValue("@Month", DateTime.ParseExact(MonthComboBox.Text, "MMMM", CultureInfo.CurrentCulture).Month);
+            cmd.Parameters.AddWithValue("@Year", Int32.Parse(YearComboBox.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable IncomeTable = new DataTable();
+            da.Fill(IncomeTable);
+            dataGridView1.DataSource = IncomeTable;
+        }
+    }
+}
