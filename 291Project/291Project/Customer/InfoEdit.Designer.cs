191e80db5b99dac678b97748ad3419811da6d73f
Tristan Bryson <brysont3@mymacewan.ca>
2018-04-08 19:09:52 -0600

    Bug fix for editing customer info

--- 291Project/291Project/Customer/InfoEdit.Designer.cs
+++ 291Project/291Project/Customer/InfoEdit.Designer.cs
@@ -30,69 +30,48 @@
         {
             this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.paymentBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            this.dataSet1 = new _291Project.dataSet1();
             this.customerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataSet1 = new _291Project.dataSet1();
+            this.paymentBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.Update_info = new System.Windows.Forms.Button();
             this.customerTableAdapter = new _291Project.dataSet1TableAdapters.CustomerTableAdapter();
             this.paymentTableAdapter = new _291Project.dataSet1TableAdapters.PaymentTableAdapter();
-            this.firstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.lastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.cityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.stateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.zipCodeDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.telephoneDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.emailDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.passwordDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.creditCardDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.AccountType = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.paymentBindingSource)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paymentBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.firstNameDataGridViewTextBoxColumn,
-            this.lastNameDataGridViewTextBoxColumn,
-            this.cityDataGridViewTextBoxColumn,
-            this.stateDataGridViewTextBoxColumn,
-            this.zipCodeDataGridViewTextBoxColumn,
-            this.telephoneDataGridViewTextBoxColumn,
-            this.emailDataGridViewTextBoxColumn,
-            this.passwordDataGridViewTextBoxColumn,
-            this.creditCardDataGridViewTextBoxColumn,
-            this.AccountType});
-            this.dataGridView1.DataSource = this.customerBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 64);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(1048, 112);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // paymentBindingSource
-            // 
-            this.paymentBindingSource.DataMember = "Payment";
-            this.paymentBindingSource.DataSource = this.dataSet1;
-            // 
-            // dataSet1
-            // 
-            this.dataSet1.DataSetName = "dataSet1";
-            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // customerBindingSource
             // 
             this.customerBindingSource.DataMember = "Customer";
             this.customerBindingSource.DataSource = this.dataSet1;
             // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "dataSet1";
+            this.dataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // paymentBindingSource
+            // 
+            this.paymentBindingSource.DataMember = "Payment";
+            this.paymentBindingSource.DataSource = this.dataSet1;
+            // 
             // Update_info
             // 
-            this.Update_info.Location = new System.Drawing.Point(417, 276);
+            this.Update_info.Location = new System.Drawing.Point(417, 301);
             this.Update_info.Name = "Update_info";
             this.Update_info.Size = new System.Drawing.Size(214, 74);
             this.Update_info.TabIndex = 1;
@@ -108,82 +87,42 @@
             // 
             this.paymentTableAdapter.ClearBeforeFill = true;
             // 
-            // firstNameDataGridViewTextBoxColumn
+            // label1
             // 
-            this.firstNameDataGridViewTextBoxColumn.DataPropertyName = "FirstName";
-            this.firstNameDataGridViewTextBoxColumn.HeaderText = "First Name";
-            this.firstNameDataGridViewTextBoxColumn.Name = "firstNameDataGridViewTextBoxColumn";
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(414, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Account Type: ";
             // 
-            // lastNameDataGridViewTextBoxColumn
+            // comboBox1
             // 
-            this.lastNameDataGridViewTextBoxColumn.DataPropertyName = "LastName";
-            this.lastNameDataGridViewTextBoxColumn.HeaderText = "Last Name";
-            this.lastNameDataGridViewTextBoxColumn.Name = "lastNameDataGridViewTextBoxColumn";
-            // 
-            // cityDataGridViewTextBoxColumn
-            // 
-            this.cityDataGridViewTextBoxColumn.DataPropertyName = "City";
-            this.cityDataGridViewTextBoxColumn.HeaderText = "City";
-            this.cityDataGridViewTextBoxColumn.Name = "cityDataGridViewTextBoxColumn";
-            // 
-            // stateDataGridViewTextBoxColumn
-            // 
-            this.stateDataGridViewTextBoxColumn.DataPropertyName = "State";
-            this.stateDataGridViewTextBoxColumn.HeaderText = "State";
-            this.stateDataGridViewTextBoxColumn.Name = "stateDataGridViewTextBoxColumn";
-            // 
-            // zipCodeDataGridViewTextBoxColumn
-            // 
-            this.zipCodeDataGridViewTextBoxColumn.DataPropertyName = "ZipCode";
-            this.zipCodeDataGridViewTextBoxColumn.HeaderText = "Zipcode";
-            this.zipCodeDataGridViewTextBoxColumn.Name = "zipCodeDataGridViewTextBoxColumn";
-            // 
-            // telephoneDataGridViewTextBoxColumn
-            // 
-            this.telephoneDataGridViewTextBoxColumn.DataPropertyName = "Telephone";
-            this.telephoneDataGridViewTextBoxColumn.HeaderText = "Telephone";
-            this.telephoneDataGridViewTextBoxColumn.Name = "telephoneDataGridViewTextBoxColumn";
-            // 
-            // emailDataGridViewTextBoxColumn
-            // 
-            this.emailDataGridViewTextBoxColumn.DataPropertyName = "Email";
-            this.emailDataGridViewTextBoxColumn.HeaderText = "Email";
-            this.emailDataGridViewTextBoxColumn.Name = "emailDataGridViewTextBoxColumn";
-            // 
-            // passwordDataGridViewTextBoxColumn
-            // 
-            this.passwordDataGridViewTextBoxColumn.DataPropertyName = "Password";
-            this.passwordDataGridViewTextBoxColumn.HeaderText = "Password";
-            this.passwordDataGridViewTextBoxColumn.Name = "passwordDataGridViewTextBoxColumn";
-            // 
-            // creditCardDataGridViewTextBoxColumn
-            // 
-            this.creditCardDataGridViewTextBoxColumn.DataPropertyName = "CreditCard";
-            this.creditCardDataGridViewTextBoxColumn.HeaderText = "Credit Card Number";
-            this.creditCardDataGridViewTextBoxColumn.Name = "creditCardDataGridViewTextBoxColumn";
-            // 
-            // AccountType
-            // 
-            this.AccountType.DataPropertyName = "AccountType";
-            this.AccountType.HeaderText = "Account Type";
-            this.AccountType.Name = "AccountType";
-            this.AccountType.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(510, 232);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // InfoEdit
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1069, 387);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.Update_info);
             this.Controls.Add(this.dataGridView1);
             this.Name = "InfoEdit";
             this.Text = "InfoEdit";
             this.Load += new System.EventHandler(this.InfoEdit_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.paymentBindingSource)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.customerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.paymentBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -196,15 +135,7 @@
         private System.Windows.Forms.Button Update_info;
         private System.Windows.Forms.BindingSource paymentBindingSource;
         private dataSet1TableAdapters.PaymentTableAdapter paymentTableAdapter;
-        private System.Windows.Forms.DataGridViewTextBoxColumn firstNameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn lastNameDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn cityDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn stateDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn zipCodeDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn telephoneDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn emailDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn passwordDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn creditCardDataGridViewTextBoxColumn;
-        private System.Windows.Forms.DataGridViewTextBoxColumn AccountType;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
--- 291Project/291Project/Customer/InfoEdit.cs
+++ 291Project/291Project/Customer/InfoEdit.cs
@@ -18,7 +18,8 @@ namespace _291Project.Customer
             InitializeComponent();
         }
 
-        private void InfoEdit_Load(object sender, EventArgs e)
+
+        private void load_info()
         {
             string str = Properties.Settings.Default.DefaultConnection;
 
@@ -28,20 +29,49 @@ namespace _291Project.Customer
             };
             con.Open();
 
-            SqlCommand cmd = new SqlCommand("SELECT CID, FirstName, LastName, City, State, ZipCode, Telephone, Email, Password, " + 
-                "AccountCreationDate, CreditCard, Rating, AccountType FROM dbo.Customer WHERE(CID = @user)", con);
+            SqlCommand cmd = new SqlCommand("SELECT FirstName, LastName, City, State, ZipCode, Telephone, Email, Password, " +
+                "CreditCard, AccountType FROM dbo.Customer WHERE(CID = @user)", con);
             cmd.Parameters.AddWithValue("@user", Program.CustomerID.ToString());
             DataTable dt = new DataTable();
             dt.Load(cmd.ExecuteReader());
             dataGridView1.DataSource = dt;
+            con.Close();
+
+         
+        }
+
+        private void InfoEdit_Load(object sender, EventArgs e)
+        {
+            load_info();
+            string str = Properties.Settings.Default.DefaultConnection;
+
+
+            SqlConnection con = new SqlConnection
+            {
+                ConnectionString = str
+            };
+            con.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("select AccountType from dbo.AccountInfo", con);
+            DataSet ds = new DataSet();
+            da.Fill(ds, "type");
+            comboBox1.DisplayMember = "AccountType";
+            comboBox1.ValueMember = "AccountType";
+            comboBox1.DataSource = ds.Tables["type"];
+            comboBox1.SelectedIndex = -1;
+            con.Close();
 
         }
 
       
         private void Update_info_Click_1(object sender, EventArgs e)
         {
-            string str = Properties.Settings.Default.DefaultConnection;
-
+            if (string.IsNullOrEmpty(comboBox1.Text))
+            {
+                MessageBox.Show("Please select an account type");
+                return;
+            }
+                string str = Properties.Settings.Default.DefaultConnection;
             SqlConnection con = new SqlConnection
             {
                 ConnectionString = str
@@ -49,7 +79,7 @@ namespace _291Project.Customer
             con.Open();
 
             SqlCommand cmd = new SqlCommand("Update dbo.Customer Set FirstName = @first, LastName = @last, City = @city, State = @state, " +
-                "ZipCode = @zip, Telephone = @phone, Email = @email, Password = @password, CreditCard = @credit, AccountType = @account WHERE(CID = @user)", con);
+                "ZipCode = @zip, Telephone = @phone, Email = @email, Password = @password, CreditCard = @credit WHERE(CID = @user)", con);
             cmd.Parameters.AddWithValue("@user", Program.CustomerID.ToString());
             cmd.Parameters.AddWithValue("@first", dataGridView1.Rows[0].Cells[0].Value);
             cmd.Parameters.AddWithValue("@last", dataGridView1.Rows[0].Cells[1].Value);
@@ -60,9 +90,32 @@ namespace _291Project.Customer
             cmd.Parameters.AddWithValue("@email", dataGridView1.Rows[0].Cells[6].Value);
             cmd.Parameters.AddWithValue("@password", dataGridView1.Rows[0].Cells[7].Value);
             cmd.Parameters.AddWithValue("@credit", dataGridView1.Rows[0].Cells[8].Value);
-            cmd.Parameters.AddWithValue("@account", dataGridView1.Rows[0].Cells[9].Value);
             DataTable dt = new DataTable();
             dt.Load(cmd.ExecuteReader());
+            load_info();
+            con.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(comboBox1.Text))
+            {
+                string selected = this.comboBox1.GetItemText(this.comboBox1.SelectedItem);
+                string str = Properties.Settings.Default.DefaultConnection;
+                SqlConnection con = new SqlConnection
+                {
+                    ConnectionString = str
+                };
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand("Update dbo.Customer Set AccountType = @account where CID = @user", con);
+                cmd.Parameters.AddWithValue("@user", Program.CustomerID.ToString());
+                cmd.Parameters.AddWithValue("@account", selected);
+                DataTable dt = new DataTable();
+                dt.Load(cmd.ExecuteReader());
+                con.Close();
+            }
+            
         }
     }
 }
